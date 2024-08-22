5d28548100f58465058445d212a3682c8d8afc05
Marcin Bojara <marcin_5@mail.ru>
2024-08-22 20:48:47 +0200

    Add password match validation and update sign-up tests
    Implemented password match validation to enable the sign-up button only when passwords match. Updated sign-up form to include new input fields and validation logic. Enhanced test suite to cover the new functionality and form elements.

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -1,11 +1,59 @@
-import SignUp from './SignUp.vue'
 import { render, screen } from '@testing-library/vue'
-import { expect } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import SignUp from './SignUp.vue'
 
-describe('Sign Up', () => {
-  it('has Sign Up header', () => {
+describe('SignUp Component', () => {
+  beforeEach(() => {
     render(SignUp)
+  })
+
+  it('has Sign Up header', () => {
     const header = screen.getByRole('heading', { name: /Sign Up/i })
     expect(header).toBeInTheDocument()
+  })
+
+  it('has username input', () => {
+    expect(screen.queryByLabelText('Username')).toBeInTheDocument()
+  })
+
+  it('has email input', () => {
+    expect(screen.queryByLabelText('Email')).toBeInTheDocument()
+  })
+
+  it('has password input', () => {
+    expect(screen.queryByLabelText('Password')).toBeInTheDocument()
+  })
+
+  it('has password type for password input', () => {
+    expect(screen.queryByLabelText('Password')).toHaveAttribute('type', 'password')
+  })
+
+  it('has password repeat input', () => {
+    expect(screen.queryByLabelText('Password repeat')).toBeInTheDocument()
+  })
+
+  it('has password type for password repeat input', () => {
+    expect(screen.queryByLabelText('Password repeat')).toHaveAttribute('type', 'password')
+  })
+
+  it('has sign up button', () => {
+    const button = screen.getByRole('button', { name: /Sign Up/i })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('disables the button initially', () => {
+    expect(screen.getByRole('button', { name: /Sign Up/i })).toBeDisabled()
+  })
+
+  describe('when user sets the same value for password inputs', () => {
+    const user = userEvent.setup()
+    it('enables the button', async () => {
+      const passwordInput = screen.getByLabelText('Password')
+      const passwordRepeatInput = screen.getByLabelText('Password repeat')
+      await user.type(passwordInput, 'password')
+      await user.type(passwordRepeatInput, 'password')
+      expect(screen.getByRole('button', { name: /Sign Up/i })).toBeEnabled()
+    })
   })
 })
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,3 +1,51 @@
 <template>
   <h1>Sign Up</h1>
+  <div>
+    <label for="username">Username</label>
+    <input id="username" />
+  </div>
+  <div>
+    <label for="email">Email</label>
+    <input id="email" />
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input id="password" type="password" @input="onPasswordInput" />
+  </div>
+  <div>
+    <label for="passwordRepeat">Password repeat</label>
+    <input id="passwordRepeat" type="password" @input="onPasswordInput" />
+  </div>
+  <button :disabled="disabled">Sign Up</button>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const PASSWORD_ID = 'password'
+const PASSWORD_REPEAT_ID = 'passwordRepeat'
+
+const disabled = ref(true)
+const password = ref('')
+const passwordRepeat = ref('')
+
+const updatePasswordValue = (id, value) => {
+  if (id === PASSWORD_ID) {
+    password.value = value
+  } else if (id === PASSWORD_REPEAT_ID) {
+    passwordRepeat.value = value
+  }
+}
+
+const handlePasswordChange = () => {
+  disabled.value = passwordRepeat.value !== password.value
+}
+
+const onPasswordInput = (event) => {
+  const { id, value } = event.target
+  if (id === PASSWORD_ID || id === PASSWORD_REPEAT_ID) {
+    updatePasswordValue(id, value)
+    handlePasswordChange()
+  }
+}
+</script>
