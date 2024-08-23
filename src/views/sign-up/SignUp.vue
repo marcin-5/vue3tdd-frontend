c5c44d5bd4a8f9a5f9ce5eb3d1a812880daa924a
Marcin Bojara <marcin_5@mail.ru>
2024-08-23 09:46:46 +0200

    Format input fields and update computed return
    Formatted input fields for consistency by adding spaces around slashes and updated the computed return logic to enhance readability. This change improves code readability and maintains a consistent coding style across the SignUp component.

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -2,25 +2,25 @@
   <h1>Sign Up</h1>
   <div>
     <label for="username">Username</label>
-    <input id="username"/>
+    <input id="username" />
   </div>
   <div>
     <label for="email">Email</label>
-    <input id="email"/>
+    <input id="email" />
   </div>
   <div>
     <label for="password">Password</label>
-    <input id="password" type="password" v-model="password"/>
+    <input id="password" type="password" v-model="password" />
   </div>
   <div>
     <label for="passwordRepeat">Password repeat</label>
-    <input id="passwordRepeat" type="password" v-model="passwordRepeat"/>
+    <input id="passwordRepeat" type="password" v-model="passwordRepeat" />
   </div>
   <button :disabled="isDisabledComputed">Sign Up</button>
 </template>
 
 <script setup>
-import {computed, ref} from 'vue'
+import { computed, ref } from 'vue'
 
 const PASSWORD_ID = 'password'
 const PASSWORD_REPEAT_ID = 'passwordRepeat'
@@ -29,6 +29,6 @@ const password = ref('')
 const passwordRepeat = ref('')
 
 const isDisabledComputed = computed(() => {
-  return (password.value || passwordRepeat.value) ? password.value !== passwordRepeat.value : true
+  return password.value || passwordRepeat.value ? password.value !== passwordRepeat.value : true
 })
 </script>
