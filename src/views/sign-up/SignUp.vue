ef7368c7bd7b19104d51863d88d4467e608254db
Marcin Bojara <marcin_5@mail.ru>
2024-08-23 08:48:32 +0200

    Fix sign up button logic and clean up code formatting
    Refactored the logic for enabling the sign up button by introducing `isDisabled` method. Removed unnecessary `handlePasswordChange` function and ensured code consistency in formatting.

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -2,32 +2,35 @@
   <h1>Sign Up</h1>
   <div>
     <label for="username">Username</label>
-    <input id="username" />
+    <input id="username"/>
   </div>
   <div>
     <label for="email">Email</label>
-    <input id="email" />
+    <input id="email"/>
   </div>
   <div>
     <label for="password">Password</label>
-    <input id="password" type="password" @input="onPasswordInput" />
+    <input id="password" type="password" @input="onPasswordInput"/>
   </div>
   <div>
     <label for="passwordRepeat">Password repeat</label>
-    <input id="passwordRepeat" type="password" @input="onPasswordInput" />
+    <input id="passwordRepeat" type="password" @input="onPasswordInput"/>
   </div>
-  <button :disabled="disabled">Sign Up</button>
+  <button :disabled="isDisabled()">Sign Up</button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 
 const PASSWORD_ID = 'password'
 const PASSWORD_REPEAT_ID = 'passwordRepeat'
 
-const disabled = ref(true)
 const password = ref('')
 const passwordRepeat = ref('')
+
+const isDisabled = () => {
+  return (password.value || passwordRepeat.value) ? password.value !== passwordRepeat.value : true
+}
 
 const updatePasswordValue = (id, value) => {
   if (id === PASSWORD_ID) {
@@ -37,15 +40,10 @@ const updatePasswordValue = (id, value) => {
   }
 }
 
-const handlePasswordChange = () => {
-  disabled.value = passwordRepeat.value !== password.value
-}
-
 const onPasswordInput = (event) => {
-  const { id, value } = event.target
+  const {id, value} = event.target
   if (id === PASSWORD_ID || id === PASSWORD_REPEAT_ID) {
     updatePasswordValue(id, value)
-    handlePasswordChange()
   }
 }
 </script>
