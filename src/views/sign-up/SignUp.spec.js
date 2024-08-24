47aed543c17907eb94a461e795d63e82784e867b
Marcin Bojara <marcin_5@mail.ru>
2024-08-24 19:34:41 +0200

    Extract constants for SignUp tests
    Moved input labels and credentials to a new file, `SignUpTestConstants.js`, to improve readability and maintainability of the `SignUp.spec.js` test file. This change consolidates repeated constant values into a single location, decreasing redundancy and simplifying updates.

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -4,20 +4,7 @@ import {render, screen, waitFor} from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import SignUp from './SignUp.vue'
 import {setupServer} from 'msw/node'
-
-const INPUT_LABELS = {
-  username: 'Username',
-  email: 'Email',
-  password: 'Password',
-  passwordRepeat: 'Password repeat',
-}
-const SIGN_UP_BUTTON_LABEL = 'Sign Up'
-const credentials = {
-  username: 'user1',
-  email: 'user1@mail.com',
-  password: 'P4ssword',
-  passwordRepeat: 'P4ssword',
-}
+import {CREDENTIALS, INPUT_LABELS, SIGN_UP_BUTTON_LABEL} from './SignUpTestConstants'
 
 const fillInput = async (label, value) => {
   const input = screen.getByLabelText(label)
@@ -100,10 +87,10 @@ describe('SignUp Component', () => {
     let button
 
     beforeEach(async () => {
-      await fillInput(INPUT_LABELS.username, credentials.username)
-      await fillInput(INPUT_LABELS.email, credentials.email)
-      await fillInput(INPUT_LABELS.password, credentials.password)
-      await fillInput(INPUT_LABELS.passwordRepeat, credentials.passwordRepeat)
+      await fillInput(INPUT_LABELS.username, CREDENTIALS.username)
+      await fillInput(INPUT_LABELS.email, CREDENTIALS.email)
+      await fillInput(INPUT_LABELS.password, CREDENTIALS.password)
+      await fillInput(INPUT_LABELS.passwordRepeat, CREDENTIALS.passwordRepeat)
       button = screen.getByRole('button', signUpButtonSelector)
       const localUser = userEvent.setup()
       await localUser.click(button)
@@ -116,9 +103,9 @@ describe('SignUp Component', () => {
     it('sends username, email, and password to the backend', async () => {
       await waitFor(() => {
         expect(requestBody).toEqual({
-          username: credentials.username,
-          email: credentials.email,
-          password: credentials.password,
+          username: CREDENTIALS.username,
+          email: CREDENTIALS.email,
+          password: CREDENTIALS.password,
         })
       })
     })
---
+++ src/views/sign-up/SignUpTestConstants.js
@@ -0,0 +1,15 @@
+export const INPUT_LABELS = {
+  username: 'Username',
+  email: 'Email',
+  password: 'Password',
+  passwordRepeat: 'Password repeat',
+}
+
+export const SIGN_UP_BUTTON_LABEL = 'Sign Up'
+
+export const CREDENTIALS = {
+  username: 'user1',
+  email: 'user1@mail.com',
+  password: 'P4ssword',
+  passwordRepeat: 'P4ssword',
+}
