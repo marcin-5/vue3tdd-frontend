cfbdfb10171126778b148c9b2e7e8820287314a4
Marcin Bojara <marcin_5@mail.ru>
2024-08-25 11:04:46 +0200

    Wrap sign-up form with <form> tag
    Encapsulated the sign-up fields and button within a <form> element to handle form submission more effectively. The @submit.prevent directive ensures the form does not reload the page on submission.

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,22 +1,24 @@
 <template>
-  <h1>Sign Up</h1>
-  <div>
-    <label for="username">Username</label>
-    <input id="username" v-model="formState.username" />
-  </div>
-  <div>
-    <label for="email">Email</label>
-    <input id="email" v-model="formState.email" />
-  </div>
-  <div>
-    <label for="password">Password</label>
-    <input id="password" type="password" v-model="formState.password" />
-  </div>
-  <div>
-    <label for="passwordRepeat">Password repeat</label>
-    <input id="passwordRepeat" type="password" v-model="formState.passwordRepeat" />
-  </div>
-  <button :disabled="isDisabledComputed" @click="submit">Sign Up</button>
+  <form @submit.prevent="submit">
+    <h1>Sign Up</h1>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" v-model="formState.username" />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input id="email" v-model="formState.email" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input id="password" type="password" v-model="formState.password" />
+    </div>
+    <div>
+      <label for="passwordRepeat">Password repeat</label>
+      <input id="passwordRepeat" type="password" v-model="formState.passwordRepeat" />
+    </div>
+    <button :disabled="isDisabledComputed">Sign Up</button>
+  </form>
 </template>
 
 <script setup>
