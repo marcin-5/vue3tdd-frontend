11ecc30406f6f2d0ba417abc3b75a010ab7c33f4
Marcin Bojara <marcin_5@mail.ru>
2024-08-28 19:37:19 +0200

    Add CustomInput component
    Introduced a new `CustomInput.vue` component for reusable input fields. The component handles input events and emits a `customInput` event with the input value. It also supports dynamic label, id, and error message properties.

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="inputWrapperClass">
+    <label :class="labelClass" :for="id">{{ label }}</label>
+    <input :class="inputClass" :id="id" @input="handleInput" />
+    <span>{{ errorMessage }}</span>
+  </div>
+</template>
+
+<script setup>
+const inputWrapperClass = 'mb-3'
+const labelClass = 'form-label'
+const inputClass = 'form-control'
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  errorMessage: String,
+})
+
+const emit = defineEmits(['customInput'])
+
+function handleInput(event) {
+  emit('customInput', event.target.value)
+}
+</script>
