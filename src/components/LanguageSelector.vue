b0f7eedeafd3be2eae173b8494c65a6eb2cf356c
Marcin Bojara <marcin_5@mail.ru>
2024-08-31 21:25:01 +0200

    Add language selector component
    Introduced a LanguageSelector component with options for Polish and English. Integrated the component into the main application layout in App.vue to enable language switching.

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="https://flagcdn.com/24x18/pl.png"
+    data-testid="language-pl-selector"
+    @click="$i18n.locale = 'pl'"
+    role="button"
+  />
+  <span class="m-1"></span>
+  <img
+    src="https://flagcdn.com/24x18/gb.png"
+    data-testid="language-en-selector"
+    @click="$i18n.locale = 'en'"
+    role="button"
+  />
+</template>
