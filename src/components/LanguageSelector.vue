65b6f33ab2c92d186dd6fe6f98df3aa26db15b87
Marcin Bojara <marcin_5@mail.ru>
2024-09-01 18:22:39 +0200

    Add alt attributes to language selector flags
    This change improves accessibility by including descriptive alt text for the Polish and English flag images in the LanguageSelector component. This ensures screen readers can convey the purpose of these images to users.

--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -3,6 +3,7 @@
     src="https://flagcdn.com/24x18/pl.png"
     data-testid="language-pl-selector"
     @click="$i18n.locale = 'pl'"
+    alt="Polish flag"
     role="button"
   />
   <span class="m-1"></span>
@@ -10,6 +11,7 @@
     src="https://flagcdn.com/24x18/gb.png"
     data-testid="language-en-selector"
     @click="$i18n.locale = 'en'"
+    alt="English flag"
     role="button"
   />
 </template>
