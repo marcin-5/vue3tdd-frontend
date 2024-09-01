09ff1869239bf5d38cd01845f240a4720e63e77e
Marcin Bojara <marcin_5@mail.ru>
2024-09-01 21:50:04 +0200

    Add i18n mocking to LanguageSelector unit tests
    Refactor the LanguageSelector unit tests to mock vue-i18n. This includes ensuring locale changes and localStorage updates are properly tested with the selected languages.

--- src/components/__tests__/LanguageSelector.unit.spec.js
+++ src/components/__tests__/LanguageSelector.unit.spec.js
@@ -1,23 +1,42 @@
 import {render, screen} from '@testing-library/vue'
 import LanguageSelector from '../LanguageSelector.vue'
 import userEvent from '@testing-library/user-event'
+import {useI18n} from 'vue-i18n'
+import {beforeEach, vi} from 'vitest'
+
+vi.mock('vue-i18n', () => ({
+  useI18n: vi.fn(),
+}))
 
 describe('Language selector', () => {
+  const mockI18n = {
+    locale: {
+      value: 'en',
+    },
+  }
+
+  beforeEach(() => {
+    useI18n.mockReturnValue(mockI18n)
+  })
+
   describe.each([{language: 'pl'}, {language: 'en'}])(
-    'when user select $language',
+    'when user selects $language',
     ({language}) => {
       it('displays expected text', async () => {
         const user = userEvent.setup()
-        const i18n = {
-          locale: 'en',
-        }
-        render(LanguageSelector, {
-          global: {
-            mocks: {$i18n: i18n},
-          },
-        })
+
+        render(LanguageSelector)
         await user.click(screen.getByTestId(`language-${language}-selector`))
-        expect(i18n.locale).toBe(language)
+        expect(mockI18n.locale.value).toBe(language)
+      })
+
+      it('stores language in localStorage', async () => {
+        const mockSetItem = vi.spyOn(Storage.prototype, 'setItem')
+        const user = userEvent.setup()
+
+        render(LanguageSelector)
+        await user.click(screen.getByTestId(`language-${language}-selector`))
+        expect(mockSetItem).toHaveBeenCalledWith('app-lang', language)
       })
     },
   )
