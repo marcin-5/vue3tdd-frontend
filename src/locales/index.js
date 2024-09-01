18e6cbf283fcc957738fb0a3c845b527c4a98a2c
Marcin Bojara <marcin_5@mail.ru>
2024-09-01 21:49:12 +0200

    Refactor i18n initialization
    Change i18n initialization to support dynamic locale retrieval. Locale is now fetched from localStorage or browser settings, defaulting to 'en'. This improves user experience by automatically using the preferred language.

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,10 +2,13 @@ import {createI18n} from 'vue-i18n'
 import en from './translations/en.json'
 import pl from './translations/pl.json'
 
-export const i18n = createI18n({
-  legacy: false,
-  globalInjection: true,
-  locale: 'pl',
-  fallbackLng: 'en',
-  messages: {en, pl},
-})
+export const createInstance = () => {
+  return createI18n({
+    legacy: false,
+    globalInjection: true,
+    locale: localStorage.getItem('app-lang') || navigator.language?.split('-')[0] || 'en',
+    messages: {en, pl},
+  })
+}
+
+export const i18n = createInstance()
