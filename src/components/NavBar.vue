3163a6cdf677a6658e8e5005397c9ed5e7b4e5fc
Marcin Bojara <marcin_5@mail.ru>
2024-09-03 20:37:33 +0200

    Add NavBar component
    Introduce a new NavBar component with home and signup links. Update the component index file to export the newly added NavBar.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar navbar-expand bg-body-tertiary shadow-sm">
+    <div class="container">
+      <router-link class="navbar-brand" data-testid="link-home-page" to="/">
+        <img
+          src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/icons/house-door.svg"
+          alt="Home logo"
+          width="32"
+        />
+        Home
+      </router-link>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" data-testid="link-signup-page" to="/signup"
+            >{{ $t('signUp') }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
