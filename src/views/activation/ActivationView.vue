8c4959e0eb612c07dee5764e54686b59728eaac7
Marcin Bojara <marcin_5@mail.ru>
2024-09-07 20:18:47 +0200

    Add onMounted API call to ActivationView
    This commit introduces an onMounted lifecycle hook to the ActivationView component, sending a PATCH request to activate the user. The user ID for the request will be parsed from the URL in a future update.

--- src/views/activation/ActivationView.vue
+++ src/views/activation/ActivationView.vue
@@ -3,3 +3,12 @@
     <h1>Activation Page</h1>
   </div>
 </template>
+
+<script setup>
+import axios from 'axios'
+import {onMounted} from 'vue'
+
+onMounted(() => {
+  axios.patch('/api/v1/users/this-will-be-taken-from-url/active')
+})
+</script>
