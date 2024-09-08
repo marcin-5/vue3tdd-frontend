5d2de0bd7f0ec6c27cdfcf512a8af34b6b3fe483
Marcin Bojara <marcin_5@mail.ru>
2024-09-08 08:39:05 +0200

    Refactor and enhance activation test suite
    Refactored the activation test suite to improve readability and maintainability by introducing constants and utility functions. Enhanced test clarity through modularization and better naming conventions.

--- src/views/activation/Activation.spec.js
+++ src/views/activation/Activation.spec.js
@@ -4,62 +4,75 @@ import {http, HttpResponse} from 'msw'
 import {afterAll, beforeAll, beforeEach} from 'vitest'
 import Activation from './ActivationView.vue'
 
-let counter = 0
-let token
-const server = setupServer(
-  http.patch('/api/v1/users/:token/active', ({params}) => {
-    counter += 1
-    token = params.token
-    return HttpResponse.json({})
-  }),
-)
+const ACTIVATION_URL = '/api/v1/users/:token/active'
+const INITIAL_PATH = '/'
+const ACTIVATION_TOKENS = ['123', '456']
 
-beforeEach(() => {
-  counter = 0
-  token = undefined
+let requestCounter = 0
+let currentActivationToken
+
+const createServer = () =>
+  setupServer(
+    http.patch(ACTIVATION_URL, ({params}) => {
+      requestCounter += 1
+      currentActivationToken = params.token
+      return HttpResponse.json({})
+    }),
+  )
+
+const server = createServer()
+
+const resetTestState = () => {
+  requestCounter = 0
+  currentActivationToken = undefined
   server.resetHandlers()
-})
+}
 
-beforeAll(() => server.listen())
-
-afterAll(() => server.close())
-
-const setup = async (path) => {
-  router.push(path)
+const setupActivation = async (path) => {
+  await router.push(path)
   await router.isReady()
   return render(Activation)
 }
 
+const verifyRequestCounter = async (expectedCount) => {
+  await waitFor(() => {
+    expect(requestCounter).toBe(expectedCount)
+  })
+}
+
+const verifyActivationToken = async (expectedToken) => {
+  await waitFor(() => {
+    expect(currentActivationToken).toBe(expectedToken)
+  })
+}
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+beforeEach(resetTestState)
+
 describe('Activation', () => {
   it('sends activation request to server', async () => {
-    await setup('/')
-    await waitFor(() => {
-      expect(counter).toBe(1)
-    })
+    await setupActivation(INITIAL_PATH)
+    await verifyRequestCounter(1)
   })
 
   describe.each([{activationToken: '123'}, {activationToken: '456'}])(
-    'when token is $token',
+    'when token is $activationToken',
     ({activationToken}) => {
       it('sends token in request', async () => {
-        await setup(`/activation/${activationToken}`)
-        await waitFor(() => {
-          expect(token).toBe(activationToken)
-        })
+        await setupActivation(`/activation/${activationToken}`)
+        await verifyActivationToken(activationToken)
       })
     },
   )
 
   describe('when token is changed', () => {
     it('sends request with new token', async () => {
-      await setup('/activation/123')
-      await waitFor(() => {
-        expect(token).toBe('123')
-      })
-      router.push('/activation/456')
-      await waitFor(() => {
-        expect(token).toBe('456')
-      })
+      await setupActivation(`/activation/${ACTIVATION_TOKENS[0]}`)
+      await verifyActivationToken(ACTIVATION_TOKENS[0])
+
+      await router.push(`/activation/${ACTIVATION_TOKENS[1]}`)
+      await verifyActivationToken(ACTIVATION_TOKENS[1])
     })
   })
 })
