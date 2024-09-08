d4afa2d96af3383541324127e70f4bb29a151659
Marcin Bojara <marcin_5@mail.ru>
2024-09-08 08:38:15 +0200

    Add token handling and routing to activation tests
    This commit enhances the activation tests by adding token handling and testing for dynamic token scenarios. The tests are updated to ensure that activation requests include the correct token, and that the system correctly handles changes in the token via routing.

--- src/views/activation/Activation.spec.js
+++ src/views/activation/Activation.spec.js
@@ -1,19 +1,22 @@
-import {render, waitFor} from 'test/helper'
+import {render, router, waitFor} from 'test/helper'
 import {setupServer} from 'msw/node'
 import {http, HttpResponse} from 'msw'
 import {afterAll, beforeAll, beforeEach} from 'vitest'
 import Activation from './ActivationView.vue'
 
 let counter = 0
+let token
 const server = setupServer(
-  http.patch('/api/v1/users/:token/active', () => {
+  http.patch('/api/v1/users/:token/active', ({params}) => {
     counter += 1
+    token = params.token
     return HttpResponse.json({})
   }),
 )
 
 beforeEach(() => {
   counter = 0
+  token = undefined
   server.resetHandlers()
 })
 
@@ -21,11 +24,42 @@ beforeAll(() => server.listen())
 
 afterAll(() => server.close())
 
+const setup = async (path) => {
+  router.push(path)
+  await router.isReady()
+  return render(Activation)
+}
+
 describe('Activation', () => {
   it('sends activation request to server', async () => {
-    render(Activation)
+    await setup('/')
     await waitFor(() => {
       expect(counter).toBe(1)
+    })
+  })
+
+  describe.each([{activationToken: '123'}, {activationToken: '456'}])(
+    'when token is $token',
+    ({activationToken}) => {
+      it('sends token in request', async () => {
+        await setup(`/activation/${activationToken}`)
+        await waitFor(() => {
+          expect(token).toBe(activationToken)
+        })
+      })
+    },
+  )
+
+  describe('when token is changed', () => {
+    it('sends request with new token', async () => {
+      await setup('/activation/123')
+      await waitFor(() => {
+        expect(token).toBe('123')
+      })
+      router.push('/activation/456')
+      await waitFor(() => {
+        expect(token).toBe('456')
+      })
     })
   })
 })
