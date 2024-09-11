beaa23493a76b7ebffc0606318c677cfd65300ed
Marcin Bojara <marcin_5@mail.ru>
2024-09-11 21:56:17 +0200

    Add AppButton component
    Created a reusable button component with customizable variants and loading state. Exported AppButton in the components index for broader use across the application.

---
+++ src/components/AppButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button :class="`btn btn-${variant}`" :disabled="isDisabled || isLoading">
+    <Spinner v-if="isLoading" />
+    <slot />
+  </button>
+</template>
+
+<script setup>
+import Spinner from './LoadingSpinner.vue'
+
+defineProps({
+  isDisabled: Boolean,
+  isLoading: Boolean,
+  variant: {
+    type: String,
+    default: 'primary',
+  },
+})
+</script>
