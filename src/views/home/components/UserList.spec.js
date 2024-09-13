456b81d8df0bc6a21b3bff55352cd76f4f496b32
Marcin Bojara <marcin_5@mail.ru>
2024-09-13 19:20:33 +0200

    Paginate user list API call
    Added pagination parameters (page, size) to the loadUsers function to support fetching paginated user lists. Updated the UserList test to handle pagination and test API responses accordingly.

--- src/views/home/components/UserList.spec.js
+++ src/views/home/components/UserList.spec.js
@@ -4,22 +4,35 @@ import {setupServer} from 'msw/node'
 import {http, HttpResponse} from 'msw'
 import {afterAll, beforeAll, beforeEach} from 'vitest'
 
-const mockedUserList = {
-  content: [
-    {id: 1, username: 'user1', email: 'user1@mail.com'},
-    {id: 2, username: 'user2', email: 'user2@mail.com'},
-    {id: 3, username: 'user3', email: 'user3@mail.com'},
-  ],
-  page: 0,
-  size: 3,
-  totalPages: 9,
+const users = [
+  {id: 1, username: 'user1', email: 'user1@mail.com'},
+  {id: 2, username: 'user2', email: 'user2@mail.com'},
+  {id: 3, username: 'user3', email: 'user3@mail.com'},
+  {id: 4, username: 'user4', email: 'user4@mail.com'},
+  {id: 5, username: 'user5', email: 'user5@mail.com'},
+  {id: 6, username: 'user6', email: 'user6@mail.com'},
+  {id: 7, username: 'user7', email: 'user7@mail.com'},
+]
+
+const getPage = (page, size) => {
+  const start = page * size
+  const end = start + size
+  return {
+    content: users.slice(start, end),
+    page,
+    size,
+    totalPages: Math.ceil(Math.ceil(page / size)),
+  }
 }
 
 const USER_API_URL = '/api/v1/users'
 
 const server = setupServer(
-  http.get(USER_API_URL, () => {
-    return HttpResponse.json(mockedUserList)
+  http.get(USER_API_URL, ({request}) => {
+    const url = new URL(request.url)
+    const size = Number.parseInt(url.searchParams.get('size')) || 5
+    const page = Number.parseInt(url.searchParams.get('page')) || 0
+    return HttpResponse.json(getPage(page, size))
   }),
 )
 
