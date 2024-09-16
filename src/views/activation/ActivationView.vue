eabe9a1dc1012207330add46872a406163eb248a
Marcin Bojara <marcin_5@mail.ru>
2024-09-16 08:45:09 +0200

    Refactor API request in ActivationView
    Replace manual API handling with useRouteParamApiRequest utility to streamline the code, improving readability and maintaining consistency. Ensure error and success messages are correctly displayed based on the updated structure.

--- src/views/activation/ActivationView.vue
+++ src/views/activation/ActivationView.vue
@@ -1,7 +1,7 @@
 <template>
   <div data-testid="activation-page">
-    <Alert v-if="status === 'success'">{{ successMessage }}</Alert>
-    <Alert v-if="status === 'fail'" variant="danger"> {{ errorMessage }}</Alert>
+    <Alert v-if="status === 'success'">{{ data.message }}</Alert>
+    <Alert v-if="status === 'fail'" variant="danger"> {{ error }}</Alert>
     <Alert variant="secondary" center v-if="status === 'loading'">
       <Spinner size="normal" />
     </Alert>
@@ -9,31 +9,9 @@
 </template>
 
 <script setup>
-import {ref, watchEffect} from 'vue'
-import {useI18n} from 'vue-i18n'
-import {useRoute} from 'vue-router'
 import {Alert, Spinner} from '@/components'
 import {activate} from '@/views/activation/api.js'
+import useRouteParamApiRequest from '@/shared/useRouteParamApiRequest.js'
 
-const {t} = useI18n()
-const route = useRoute()
-const errorMessage = ref()
-const successMessage = ref()
-const status = ref('')
-
-watchEffect(async () => {
-  status.value = 'loading'
-  try {
-    const response = await activate(route.params.token)
-    successMessage.value = response.data.message
-    status.value = 'success'
-  } catch (apiError) {
-    if (apiError.response?.data?.message) {
-      errorMessage.value = apiError.response.data.message
-    } else {
-      errorMessage.value = t('genericError')
-    }
-    status.value = 'fail'
-  }
-})
+const {status, data, error} = useRouteParamApiRequest(activate, 'token')
 </script>
