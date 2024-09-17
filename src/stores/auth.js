6c6fae35904e8b0f47d3384068b5b2cb560ec338
Marcin Bojara <marcin_5@mail.ru>
2024-09-17 19:03:11 +0200

    Persist auth state in localStorage.
    Added logic to persist the authentication state in localStorage by watching changes in the auth reactive object. This ensures that on page refresh, the auth state is maintained by initializing it from localStorage if available.

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,18 +1,40 @@
 import {defineStore} from 'pinia'
-import {reactive} from 'vue'
+import {reactive, watch} from 'vue'
 
-const initialAuthState = {
+const INITIAL_AUTH_STATE = {
   id: 0,
   username: '',
   email: '',
 }
 
+function getInitialAuthState() {
+  const storedState = localStorage.getItem('auth')
+  if (storedState !== null) {
+    try {
+      return JSON.parse(storedState)
+    } catch (error) {
+      console.error('Failed to parse stored auth state:', error)
+    }
+  }
+  return {}
+}
+
+const initialState = {...INITIAL_AUTH_STATE, ...getInitialAuthState()}
+
 export const useAuthStore = defineStore('auth', () => {
-  const auth = reactive({...initialAuthState})
+  const auth = reactive({...initialState})
 
   function updateAuthState(data) {
     Object.assign(auth, data)
   }
+
+  watch(
+    auth,
+    () => {
+      localStorage.setItem('auth', JSON.stringify(auth))
+    },
+    {deep: true},
+  )
 
   return {auth, updateAuthState}
 })
