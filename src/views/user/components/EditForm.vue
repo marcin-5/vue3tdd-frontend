5004aa9f9e052045b4838f1549d69b23343ec423
Marcin Bojara <marcin_5@mail.ru>
2024-09-19 21:19:24 +0200

    Add user edit form component
    Introduced a new EditForm component for user settings. This form includes input for username and buttons to save or cancel edits, improving user experience.

---
+++ src/views/user/components/EditForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <AppInput id="username" :label="$t('username')" v-model="authState.username" />
+  <AppButton>{{ $t('save') }}</AppButton>
+  <div class="d-inline m-1"></div>
+  <AppButton variant="outline-secondary" @click="$emit('cancel')">{{ $t('cancel') }}</AppButton>
+</template>
+
+<script setup>
+import {AppButton, AppInput} from '@/components'
+import {useAuthStore} from '@/stores/auth'
+
+defineEmits(['cancel'])
+const {authState} = useAuthStore()
+</script>
