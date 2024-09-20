8e772d650a0ceee3ff3c51a0d6528e8bcb847314
Marcin Bojara <marcin_5@mail.ru>
2024-09-20 08:19:30 +0200

    Update username display logic in ProfileCard.vue
    Changed the username display logic to use a computed property that checks the authState. Also, added an event handler to the save event in the EditForm component to exit edit mode after saving.

--- src/views/user/components/ProfileCard.vue
+++ src/views/user/components/ProfileCard.vue
@@ -12,14 +12,14 @@
     <template v-slot:body>
       <div class="text-center">
         <template v-if="!editMode">
-          <h3>{{ user.username }}</h3>
+          <h3>{{ username }}</h3>
           <AppButton v-if="authState.id === user.id" @click="editMode = !editMode"
             >{{ $t('edit') }}
           </AppButton>
           <div class="mt-3"></div>
           <UserDeleteButton :id="user.id" />
         </template>
-        <EditForm v-if="editMode" @cancel="editMode = false" />
+        <EditForm v-if="editMode" @cancel="editMode = false" @save="editMode = false" />
       </div>
     </template>
   </Card>
@@ -30,12 +30,15 @@ import {AppButton, Card} from '@/components'
 import UserDeleteButton from './UserDeleteButton.vue'
 import EditForm from './EditForm.vue'
 import {useAuthStore} from '@/stores/auth.js'
-import {ref} from 'vue'
+import {computed, ref} from 'vue'
 
-defineProps({
+const props = defineProps({
   user: Object,
 })
 
 const editMode = ref(false)
 const {authState} = useAuthStore()
+const username = computed(() =>
+  authState.id === props.user.id ? authState.username : props.user.username,
+)
 </script>
