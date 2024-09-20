95fd802e3dd51718ad8eb750c1151975a1c2f5a0
Marcin Bojara <marcin_5@mail.ru>
2024-09-20 09:09:55 +0200

    Add user update functionality
    Introduced `updateUser` API method for user details update. Modified `EditForm.vue` to include a form submission handler and loading state to manage the update process.

--- src/views/user/components/EditForm.vue
+++ src/views/user/components/EditForm.vue
@@ -1,14 +1,27 @@
 <template>
-  <AppInput id="username" :label="$t('username')" v-model="authState.username" />
-  <AppButton>{{ $t('save') }}</AppButton>
-  <div class="d-inline m-1"></div>
-  <AppButton variant="outline-secondary" @click="$emit('cancel')">{{ $t('cancel') }}</AppButton>
+  <form @submit.prevent="submit">
+    <AppInput id="username" :label="$t('username')" v-model="authState.username" />
+    <AppButton type="submit" :is-loading="isLoading">{{ $t('save') }}</AppButton>
+    <div class="d-inline m-1"></div>
+    <AppButton type="button" variant="outline-secondary" @click="$emit('cancel')"
+      >{{ $t('cancel') }}
+    </AppButton>
+  </form>
 </template>
 
 <script setup>
 import {AppButton, AppInput} from '@/components'
 import {useAuthStore} from '@/stores/auth'
+import {ref} from 'vue'
+import {updateUser} from './api'
 
-defineEmits(['cancel'])
+const emit = defineEmits(['cancel', 'save'])
 const {authState} = useAuthStore()
+const isLoading = ref(false)
+
+const submit = async () => {
+  isLoading.value = true
+  await updateUser(authState.id, {username: authState.username})
+  emit('save')
+}
 </script>
