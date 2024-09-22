4fa96c0a037adb77ebeabd5074afed6dbef83061
Marcin Bojara <marcin_5@mail.ru>
2024-09-22 11:13:52 +0200

    Improve error handling in EditForm.vue
    Refactor the form submission to include detailed error handling. Introduce display of validation errors and a general error alert. Rename variables for clarity and align with new functionality.

--- src/views/user/components/EditForm.vue
+++ src/views/user/components/EditForm.vue
@@ -1,7 +1,13 @@
 <template>
-  <form @submit.prevent="submit">
-    <AppInput id="username" :label="$t('username')" v-model="username" />
-    <AppButton type="submit" :is-loading="isLoading">{{ $t('save') }}</AppButton>
+  <form @submit.prevent="handleSubmit">
+    <AppInput
+      id="username"
+      :label="$t('username')"
+      v-model="username"
+      :error-message="validationErrors.username"
+    />
+    <Alert v-if="generalError" variant="danger">{{ generalError }}</Alert>
+    <AppButton type="submit" :in-progress="isSaving">{{ $t('save') }}</AppButton>
     <div class="d-inline m-1"></div>
     <AppButton type="button" variant="outline-secondary" @click="$emit('cancel')"
       >{{ $t('cancel') }}
@@ -10,20 +16,49 @@
 </template>
 
 <script setup>
-import {AppButton, AppInput} from '@/components'
+import {Alert, AppButton, AppInput} from '@/components'
 import {useAuthStore} from '@/stores/auth'
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
 import {updateUser} from './api'
+import {useI18n} from 'vue-i18n'
 
+const {t} = useI18n()
 const emit = defineEmits(['cancel', 'save'])
 const {authState, updateAuthState} = useAuthStore()
-const isLoading = ref(false)
-const username = ref(authState.username)
 
-const submit = async () => {
-  isLoading.value = true
-  await updateUser(authState.id, {username: username.value})
-  updateAuthState({username: username.value})
-  emit('save')
+const isSaving = ref(false)
+const username = ref(authState.username)
+const generalError = ref()
+const validationErrors = ref({})
+
+const userId = authState.id
+
+const handleSubmit = async () => {
+  isSaving.value = true
+  generalError.value = undefined
+  try {
+    await updateUser(userId, {username: username.value})
+    updateAuthState({username: username.value})
+    emit('save')
+  } catch (apiError) {
+    handleError(apiError)
+  } finally {
+    isSaving.value = false
+  }
 }
+
+const handleError = (error) => {
+  if (error.response?.status === 400) {
+    validationErrors.value = error.response.data.validationErrors
+  } else {
+    generalError.value = t('genericError')
+  }
+}
+
+watch(
+  () => username.value,
+  () => {
+    delete validationErrors.value.username
+  },
+)
 </script>
