618342495afd2caa782bb01e4ea6e32b69474af2
Marcin Bojara <marcin_5@mail.ru>
2024-09-27 14:27:10 +0200

    Update profile image path in UserItem component
    The profile image path has been modified to reference a different location, ensuring the image is correctly loaded.

--- src/views/home/components/UserItem.vue
+++ src/views/home/components/UserItem.vue
@@ -1,6 +1,11 @@
 <template>
   <li class="list-group-item list-group-item-action" @click="router.push('/user/' + user.id)">
-    <img src="@/assets/profile.png" alt="Profile" width="30" class="rounded-circle shadow-sm" />
+    <img
+      src="../../../../public/assets/profile.png"
+      alt="Profile"
+      width="30"
+      class="rounded-circle shadow-sm"
+    />
     {{ user.username }}
   </li>
 </template>
