e0d3053414854edba121476969301a1ee23678ba
Marcin Bojara <marcin_5@mail.ru>
2024-09-27 16:11:49 +0200

    Add ProfileImage component with unit tests
    Implemented the ProfileImage component, including handling for default, user-provided, and temporary images. Created comprehensive unit tests to ensure correct image display logic based on props. Updated the component export file to include the new ProfileImage component.

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <img alt="image" :src="imagePath" />
+</template>
+
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({
+  image: String,
+  tempImage: String,
+})
+
+const getImagePath = () => {
+  if (props.tempImage) return props.tempImage
+  if (props.image) return `/images/${props.image}`
+  return '/assets/profile.png'
+}
+
+const imagePath = computed(getImagePath)
+</script>
